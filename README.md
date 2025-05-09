9eaa76ab7b93b871ea2e7b9ffddc717599a22694
Brad <bradekuhnle@gmail.com>
2025-05-08 19:55:20 -0500

    added ReadMe'

---
+++ README.md
@@ -0,0 +1,7 @@
+# this the my text adventure game that i am working on.
+
+---
+this will be an open sorce project from now on. i will be updating and adding new things to the game.
+if anyone wants to help out with some items or enemies you can reach out to me at constrictdegaming@gmail.com.
+
+aslo please forgive me for any spelling errors i have dyslexia.
--- obj/Debug/net9.0/text adventer rouge-like.AssemblyInfo.cs
+++ obj/Debug/net9.0/text adventer rouge-like.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text adventer rouge-like")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d073bb80b2aa619ac7075f7abefd43b1b6ac9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("text adventer rouge-like")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text adventer rouge-like")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
